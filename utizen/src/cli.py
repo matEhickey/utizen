442cbde4cdba81427f63a2510122bff0085ab6fb
matEhickey <mathias.didier7@wanadoo.fr>
2020-09-25 13:02:09 +0200

    add uninstall all apps

--- utizen/src/cli.py
+++ utizen/src/cli.py
@@ -13,32 +13,47 @@ from utils import *
 @click.group()
 def cli():
     pass
-    
+
 @cli.command()
 @click.argument('config', type=click.STRING, autocompletion=get_configs_names_autocomplete, metavar='<config name>')
 def install(config):
     ip, port = get_connected_tv_ip_port()
     run(config, ip, port)
-    
+
 @cli.command()
 @click.argument('config', type=click.STRING, autocompletion=get_configs_names_autocomplete, metavar='<config name>')
 def config(config):
     app, filename = get_config(config)
     click.echo(pformat(app))
-    
+
 @cli.command()
 def ls():
     for i in get_configs_names():
         click.echo(i)
-        
+
 @cli.command()
 @click.argument('config', type=click.STRING, autocompletion=get_configs_names_autocomplete, metavar='<config name>')
 def uninstall(config):
+    configs = []
+    if(config == "all"):
+        print("Uninstalling all app from TV")
+        configs = get_configs_names()
+    else:
+        configs.append(config)
+
+    for i in configs:
+        uninstall_app(i)
+
+def uninstall_app(config):
     app, filename = get_config(config)
     app_name = app["app_name"]
     ip, port = get_connected_tv_ip_port()
-    
+
     installed_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "installed", ip, "{}.txt".format(app_name))
+
+    if(not os.path.exists(installed_filename)):
+        return
+
     with open(installed_filename) as installed_file:
         for app_id in installed_file.readlines():
             app_id = app_id.replace("\n", "")
@@ -47,13 +62,13 @@ def uninstall(config):
             out, err = execute_cmd(uninstall_command)
             click.echo(out)
     os.remove(installed_filename)
-                
+
 @cli.command()
 @click.option('--name', prompt='App name (camelCaseWithoutSymbols)', help='The name of the app (camelCaseWithoutSymbols)')
 @click.option('--app_path', default= '.', help='The path of the app')
 def create(name, app_path):
     path = os.path.abspath(os.path.join(os.getcwd(), app_path))
-    
+
     content = {
         "app_name": name,
         "app_path": path,
@@ -62,7 +77,7 @@ def create(name, app_path):
             "http://tizen.org/privilege/internet"
         ]
     }
-    
+
     save_config_file(name, content)
 
 @cli.command()
@@ -76,13 +91,13 @@ def set_privileges(config):
     title = 'Choose the privileges to set to the app (press SPACE to mark, ENTER to continue):'
     options = get_all_privileges()
     selected = pick.pick(options, title, multiselect=True, min_selection_count=0)
-    
+
     privileges = [s[0] for s in selected]
     click.secho("Will set the following privileges to {}:".format(config), bg='red', fg='white')
     for i in privileges:
         click.secho(i)
-    
-    
+
+
     click.secho("Are you sure ?", bg='red', fg='white')
     if not click.confirm(""):
         click.secho("Aborted", bg='black', fg='white')
@@ -101,6 +116,6 @@ def show_privileges(config):
 # autocomplete need this to be defined (when use globaly, __name__ != __main__)
 # for command in  [install, uninstall, create, tv, set_privileges, show_privileges]:
 #     cli.add_command(command)
-    
+
 if(__name__=="__main__"):
     cli()
--- utizen/src/utils.py
+++ utizen/src/utils.py
@@ -9,11 +9,11 @@ import xml.etree.ElementTree as ET
 def get_configs_files():
     glob_configs = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "configs", "projects", "*.json")
     return glob.glob(glob_configs)
-    
+
 def get_configs_names():
     config_files = get_configs_files()
     return map(lambda x: x.split("/")[-1].split(".")[0], config_files)
-    
+
 def get_configs_names_autocomplete(ctx, args, incomplete):
     config_names = get_configs_names()
     return filter(lambda x: x.startswith(incomplete), config_names)
@@ -26,7 +26,7 @@ def get_app_id(package_tmp):
     root = parseXML(filename)
     app_id = filter(lambda x: x[0] == "id", root.getchildren()[0].items())[0][1]
     return app_id
-    
+
 def execute_cmd(cmd):
     # proc = subprocess.Popen(cmd, shell=True)
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -42,7 +42,7 @@ def store_uploaded_app(app_id, app_name, ip):
     dirname = os.path.abspath(os.path.abspath(os.path.join(filename, os.pardir)))
     if not os.path.exists(dirname):
         os.makedirs(dirname)
-        
+
     execute_cmd("echo '{}' >> {}".format(app_id, filename))
 
 def get_connected_tv_ip_port():
@@ -55,32 +55,32 @@ def get_connected_tv_ip_port():
         sys.exit(-1)
 
 def get_config(config_name):
-    filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "configs", "projects", "{}.json".format(config_name))  
-    
-    config = {}  
+    filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "configs", "projects", "{}.json".format(config_name))
+
+    config = {}
     with open(filename) as f:
         content = json.loads(f.read())
         config["app_name"] = str(content["app_name"])
         config["app_path"] = str(content["app_path"])
         config["privileges"] = content["privileges"] if "privileges" in content else []
-    
+
     return config, filename
 
 def save_config_file(name, content):
-        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "configs", "projects", "{}.json".format(name)) 
+        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "configs", "projects", "{}.json".format(name))
         filecontent = json.dumps(content, sort_keys=True, indent=4)
-        
+
         dirname = os.path.abspath(os.path.abspath(os.path.join(filename, os.pardir)))
         if not os.path.exists(dirname):
             os.makedirs(dirname)
-        
+
         f = open(filename, "w")
         f.write(filecontent)
         f.close()
-        
+
         print("* Config file created at {}".format(filename))
-        
-        
+
+
 def get_all_privileges():
     privilege_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir, "configs", "privileges.txt")
     f = open(privilege_file)
